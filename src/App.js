b85930706fdcefb147015137f91ac4d381ffa231
lynda <ahlem@gmail.com>
2021-08-09 13:05:43 +0100

    add bookCard component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import './App.css';
 import Header from './components/Header'
-import Filters from './components/Filters'
-import Books from './components/Books'
+// import Filters from './components/Filters'
+// import Books from './components/Books'
+import BookCard from './components/BookCard';
+
+import img from './assets/books.jpeg'
 
 function App() {
   return (
     <div className="App">
      <Header title="BOOKS"/>
      <div className="content">
-        <Filters styles={{ flex: 1, backgroundColor: "black"}}/>
-        <Books styles={{ flex: 2}}/>
+        <BookCard
+          img={img}
+          title="book title"
+          author="Sara"
+          rating={4}
+          voters={30}
+          description="book description" />
+        {/* <Filters styles={{ flex: 1, backgroundColor: "black"}}/>
+        <Books styles={{ flex: 2}}/> */}
      </div>
     </div>
   );
--- src/components/BookCard/index.js
+++ src/components/BookCard/index.js
@@ -1,9 +1,24 @@
 import "./bookCard.css"
+import { Rate } from "antd"
 
-const BookCard = () => {
+const BookCard = ({title, author, img, rating, voters, description}) => {
+
     return (
-        <div>
-
+        <div className="book-card-container">
+            <div className="book-img-container">
+                <img src={img} width="140" height="220" alt="book-img"/>
+            </div>
+            <div className="book-content-container">
+                <div className="book-title">{title}</div>
+                <div className="book-author">{`By ${author}`}</div>
+                <div className="rating-container">
+                    <div className="book-rating">
+                        <Rate style={{fontSize: 12}} disabled value={rating}/>
+                    </div>
+                    <div className="book-voters">{`${voters} Voters`}</div>
+                </div>
+                <div className="book-description">{description}</div>
+            </div>
         </div>
     )
 }
