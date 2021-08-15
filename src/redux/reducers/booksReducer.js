e7d5e3b09702d62d82ec5c729292fd06a53f6e9f
lynda <ahlem@gmail.com>
2021-08-15 11:10:01 +0100

    add actions to add_New_Book

--- src/redux/reducers/booksReducer.js
+++ src/redux/reducers/booksReducer.js
@@ -12,9 +12,16 @@ const initialState = {
 }
 const bookReducer = (state=initialState, action) => {
     switch (action.type) {
-        case "SET_BOOKS": {
+        case "SET_BOOKS": 
             return {...state, books: action.payload }
-        }
+        case "UPDATE_VISIBLE":
+            return {...state, visible: action.payload}
+        case "UPDATE_NEW_BOOK":
+            return {...state, newBook: action.payload}
+        case "ADD_NEW_BOOK":
+            return {...state, books: [...state.books, action.payload]}
+        case "UPDATE_ERR":
+            return {...state, error: action.payload}
         default:
             return state
     }
