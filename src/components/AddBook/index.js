64a471d0c08792a35c718eeb4426585b8176365f
lynda <ahlem@gmail.com>
2021-08-15 11:10:19 +0100

    add a new book using axios

--- src/components/AddBook/index.js
+++ src/components/AddBook/index.js
@@ -1,46 +1,83 @@
 import "./addBook.css"
 import { Button, Modal, Input, Rate } from "antd"
+import { useDispatch, useSelector } from "react-redux"
+import axios from "axios"
 
 const AddBook = () => {
+    const dispatch = useDispatch()
+    const { visible, newBook} = useSelector(state=> state.books)
+    const postBookToDB = async (newBook) => {
+        try {
+            const response = await axios.post("http://localhost:10010/books", newBook)
+            if(!response || response.status !== 201) {
+                return dispatch({type: "UPDATE_ERR", payload: "FAILED TO ADD"})
+            }
+            console.log("In add book ", response.data.data)
+            dispatch({type: "ADD_NEW_BOOK", payload: response.data.data })
+
+        } catch(err) {
+            console.log("err: ", err)
+            return dispatch({type: "UPDATE_ERR", payload: "FAILED TO ADD"})
+        }
+    }
     return (
         <div>
             <Button
                 size="large"
-                // onClick={()=> }
+                onClick={()=> dispatch({ type: "UPDATE_VISIBLE", payload: true })}
                 >
                 Add New Book
             </Button>
             <Modal
                 title="Add New Book"
-                // visible={}
-                // onCancel={}
-                // onOk={}
+                visible={visible}
+                onCancel={()=> dispatch({ type: "UPDATE_VISIBLE", payload: false })}
+                onOk={() => {
+                    postBookToDB(newBook)
+                    dispatch({ type: "UPDATE_VISIBLE", payload: false })
+                }}
                 >
                 <Input 
                     style={{ margin: 10}}
                     placeholder="Book Title"
                     size="large"
-                    // onChange={(evt) => }
-                    // value={}
+                    onChange={(event) => 
+                        dispatch({
+                            type: "UPDATE_NEW_BOOK",
+                            payload: {...newBook, title: event.target.value }
+                        }) }
+                    value={newBook.title}
                     />
                 <Input 
                     style={{ margin: 10}}
                     placeholder="Book Author"
                     size="large"
-                    // onChange={(evt) => }
-                    // value={}
+                    onChange={(event) => 
+                        dispatch({
+                            type: "UPDATE_NEW_BOOK",
+                            payload: {...newBook, author: event.target.value}
+                        })}
+                    value={newBook.author}
                     />
                 <Input 
                     style={{ margin: 10}}
                     placeholder="Book Description"
                     size="large"
-                    // onChange={(evt) => }
-                    // value={}
+                    onChange={(event) =>
+                        dispatch({
+                            type: "UPDATE_NEW_BOOK",
+                            payload: {...newBook, description: event.target.value}
+                        }) }
+                    value={newBook.description}
                     />
                 <Rate 
                     style={{ margin: 10}}
-                     // onChange={(evt) => }
-                    // value={}
+                     onChange={(value) => 
+                        dispatch({
+                            type: "UPDATE_NEW_BOOK",
+                            payload: {...newBook, rating: value}
+                        }) }
+                     value={newBook.rating}
                     />
             </Modal>
         </div>
